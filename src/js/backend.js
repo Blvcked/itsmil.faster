d1044275212b7fbfcc15c33a20b9d59767784a9b
Miles <98649838+Blvcked@users.noreply.github.com>
2023-11-17 23:23:25 +0100

    Modifying Native WP Blocks with React

--- src/js/backend.js
+++ src/js/backend.js
@@ -33,7 +33,8 @@ const DataAttributeManager = ({ customDataAttrs, setAttributes }) => {
     const [newAttrValue, setNewAttrValue] = useState("");
 
     const addDataAttribute = () => {
-        if (newAttrName && newAttrValue) {
+        if (newAttrName) {
+            // Only check for attribute name, value can be empty
             const newDataAttrs = [
                 ...customDataAttrs,
                 { name: newAttrName, value: newAttrValue },
@@ -60,64 +61,81 @@ const DataAttributeManager = ({ customDataAttrs, setAttributes }) => {
         <>
             <Flex
                 direction="column"
-                gap="4"
+                gap="6"
             >
                 {customDataAttrs.map((attr, index) => (
                     <Flex
                         key={index}
-                        gap="4"
+                        direction="column"
+                        gap="0"
                     >
-                        <FlexBlock>
-                            <TextControl
-                                label="Attribute Name"
-                                value={attr.name}
-                                onChange={(value) =>
-                                    updateDataAttribute(index, "name", value)
-                                }
-                            />
-                        </FlexBlock>
-                        <FlexBlock>
-                            <TextControl
-                                label="Attribute Value"
-                                value={attr.value}
-                                onChange={(value) =>
-                                    updateDataAttribute(index, "value", value)
-                                }
-                            />
-                        </FlexBlock>
-                        <FlexItem>
-                            <Button
-                                isDestructive
-                                onClick={() => removeDataAttribute(index)}
-                            >
-                                Remove
-                            </Button>
-                        </FlexItem>
+                        <Flex>
+                            <FlexBlock>
+                                <TextControl
+                                    label="Attribute"
+                                    value={attr.name}
+                                    onChange={(value) =>
+                                        updateDataAttribute(
+                                            index,
+                                            "name",
+                                            value
+                                        )
+                                    }
+                                />
+                            </FlexBlock>
+                            <FlexBlock>
+                                <TextControl
+                                    label="Value"
+                                    value={attr.value}
+                                    onChange={(value) =>
+                                        updateDataAttribute(
+                                            index,
+                                            "value",
+                                            value
+                                        )
+                                    }
+                                />
+                            </FlexBlock>
+                        </Flex>
+                        <Button
+                            isSecondary
+                            onClick={() => removeDataAttribute(index)}
+                            style={{ justifyContent: "center" }}
+                        >
+                            Remove Attribute
+                        </Button>
                     </Flex>
                 ))}
+                {/* Inputs for adding a new attribute */}
+                <Flex
+                    direction="column"
+                    gap="0"
+                >
+                    <Flex>
+                        <FlexBlock>
+                            <TextControl
+                                label="Attribute"
+                                value={newAttrName}
+                                onChange={setNewAttrName}
+                            />
+                        </FlexBlock>
+                        <FlexBlock>
+                            <TextControl
+                                label="Value"
+                                value={newAttrValue}
+                                onChange={setNewAttrValue}
+                            />
+                        </FlexBlock>
+                    </Flex>
+                    <Button
+                        isPrimary
+                        onClick={addDataAttribute}
+                        style={{ justifyContent: "center" }}
+                    >
+                        Add New Attribute
+                    </Button>
+                </Flex>
             </Flex>
-            <Flex gap="4">
-                <FlexBlock>
-                    <TextControl
-                        label="Attribute Name"
-                        value={newAttrName}
-                        onChange={setNewAttrName}
-                    />
-                </FlexBlock>
-                <FlexBlock>
-                    <TextControl
-                        label="Attribute Value"
-                        value={newAttrValue}
-                        onChange={setNewAttrValue}
-                    />
-                </FlexBlock>
-            </Flex>
-            <Button
-                isPrimary
-                onClick={addDataAttribute}
-            >
-                Add New Attribute
-            </Button>
         </>
     );
 };
@@ -131,8 +149,8 @@ const withInspectorControl = createHigherOrderComponent((BlockEdit) => {
                 <BlockEdit {...props} />
                 <InspectorControls>
                     <PanelBody
-                        title="Custom Data Attributes"
-                        initialOpen={true}
+                        title="Data Attributes"
+                        initialOpen={false}
                     >
                         <DataAttributeManager
                             customDataAttrs={props.attributes.customDataAttrs}
@@ -158,8 +176,9 @@ addFilter(
 function addDataToSaveElement(extraProps, blockType, attributes) {
     // if (blockType.name === "core/paragraph") {
     attributes.customDataAttrs.forEach(({ name, value }) => {
-        if (name && value) {
-            extraProps[name] = value;
+        if (name) {
+            // Only check for attribute name, value can be empty
+            extraProps[name] = value || "";
         }
     });
     // }
