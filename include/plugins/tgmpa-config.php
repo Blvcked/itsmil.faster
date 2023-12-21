8e5fa07fc7acbb6a70cbc39d805ca4d57f4bc26e
Miles <98649838+Blvcked@users.noreply.github.com>
2023-12-21 22:40:10 +0100

    update WS Form links

--- include/plugins/tgmpa-config.php
+++ include/plugins/tgmpa-config.php
@@ -23,7 +23,7 @@ function seb_theme_plugins() {
         array(
             'name'     => 'WS Form PRO',
             'slug'     => 'ws-form-pro',
-            'source'   => 'https://wsform.com/index.php?eddfile=163977%3A498%3A1&ttl=1700274027&file=1&token=bf0b43f0de3652615365522774ccf6372304d4b1a9fa96d6b6b9262e9808fd9a',
+            'source'   => 'https://wsform.com/index.php?eddfile=172337%3A498%3A1&ttl=1703258600&file=1&token=fd467bdb270f0cd86d509669732ea0a5da8fe99449a42d1836d87d57539c2024',
             'required' => false
         ),
 
@@ -31,7 +31,7 @@ function seb_theme_plugins() {
         array(
             'name'     => 'WS Form PRO - User Management',
             'slug'     => 'ws-form-user',
-            'source'   => 'https://wsform.com/index.php?eddfile=163977%3A650%3A1&ttl=1700274027&file=1&token=ba128bed97e22e75e791d20907c3f39fb53fc1640806bad57e017bd869a0af9d',
+            'source'   => 'https://wsform.com/index.php?eddfile=172337%3A650%3A1&ttl=1703258600&file=1&token=9408e8ab1febf9f11f35f796630a59a8736831b4f66862fc4e29c6bb5b6f5e88',
             'required' => false
         ),
 
